f6c097399e23e1140c0228b992dfb8f54b75bac9
arsh <arshdeepkhalsa60@gmail.com>
2026-01-07 17:19:44 +0530

    initial commit

--- README.md
+++ README.md
@@ -40,8 +40,4 @@ Built with next.js, [shadcn/ui](https://ui.shadcn.com/), and [magic ui](https://
    pnpm dev
    ```
 
-5. Open the [Config file](./src/data/resume.tsx) and make changes
-
-# License
-
-Licensed under the [MIT license](https://github.com/dillionverma/portfolio/blob/main/LICENSE.md).
+5. Open the [Config file](./src/data/resume.tsx) and make changes 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -158,18 +158,18 @@ export default function Page() {
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
                 <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-                  Hackathons
+                  Hackathons & Open Sources
                 </div>
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                  I like building things
+                  I like building things and contibuting
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                   During my time in university, I attended{" "}
-                  {DATA.hackathons.length}+ hackathons. People from around the
-                  country would come together and build incredible things in 2-3
-                  days. It was eye-opening to see the endless possibilities
-                  brought to life by a group of motivated and passionate
-                  individuals.
+                  2 hackathons. People from dfferent backgrounds came together
+                  to create innovative solutions in a short amount of time.
+                  It was really an eye opener to me. Apart from this I contributed in two of the 
+                  popular OpenSources HacktoberFest and GirlScript Summer of Code.
+
                 </p>
               </div>
             </div>
